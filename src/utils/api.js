4763376f790dc59a7652c4a0c0e5fdd7f6b52a40
Marcelo Marques <marcelo-mls@hotmail.com>
2024-09-19 00:52:18 -0300

    fix handle apis erros

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,29 @@
-const url = 'https://api-qa-spaces-target.onrender.com';
-// const url = 'http://localhost:3001';
+// const url = 'https://api-qa-spaces-target.onrender.com';
+const url = 'http://localhost:3001';
 
 export async function fetchSpaceContent(space) {
+  const errorMessage = '😬🛠️ Desculpe, um erro inesperado ocorreu. Por favor, tente novamente em alguns instantes.';
+  
   try {
     const config = {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     };
 
+
     const endpoint = `${url}/space/${space}`;
     const response = await fetch(endpoint, config);
     const data = await response.json();
 
+    if (!response.ok) {
+      console.info(response.status);
+      return {status: response.status, message: data.message || errorMessage};
+    }
+
     return data;
+
   } catch (error) {
     console.error(error);
-    return {status: 500, message: 'Parece ser um erro interno do servidor. Tente recarregar a página.'};
+    return {status: 500, message: errorMessage};
   }
 }
